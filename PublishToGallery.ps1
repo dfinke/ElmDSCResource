04a9dbe86c629a381be19c5a21bbda8fafec8581
dfinke <finked@hotmail.com>
2015-09-17 13:24:48 -0400

    Update

--- PublishToGallery.ps1
+++ PublishToGallery.ps1
@@ -1,10 +1,10 @@
 $p = @{
     Name = "ElmDSCResource"
     NuGetApiKey = $NuGetApiKey
-    LicenseUri = ""
+    LicenseUri = "https://raw.githubusercontent.com/dfinke/ElmDSCResource/master/LICENSE.txt"
     Tag = "Elm","DSC"
-    ReleaseNote = "Compiles files written in the Elm language"
-    ProjectUri = ""
+    ReleaseNote = "PowerShell DSC Resource provider that compiles files written in the Elm language"
+    ProjectUri = "https://github.com/dfinke/ElmDSCResource"
 }
 
 Publish-Module @p
